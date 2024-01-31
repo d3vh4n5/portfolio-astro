873d60b9d9cf9d6805ac1c27f6ae50c79bba2630
Juan <juanangelbasgall@hotmail.com>
2024-01-30 21:41:02 -0300

    Reparado error del select language

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,14 +1,9 @@
 import spanish from './es.json'
 import english from './en.json'
 
-import { HERO } from '@/data/hero.data'
-
-const ENGLISH_CONTENT = {...spanish, ...HERO['en']}
-const SPANISH_CONTENT = {...spanish, ...HERO['es']}
-
 export const LANGUAGES = {
-    en: ENGLISH_CONTENT,
-    es: SPANISH_CONTENT
+    en: english,
+    es: spanish
 }
 
 
