ffb840a9729e58eaa1920dafa5894319f9fbbda8
Juan <juanangelbasgall@hotmail.com>
2024-02-01 15:09:04 -0300

    Agregas redes sociales al footer

---
+++ src/constants/userLinks.constants.js
@@ -0,0 +1,5 @@
+export const userLinks = {
+    resume: "https://firebasestorage.googleapis.com/v0/b/portfolio-c7261.appspot.com/o/files%2FCV_Juan_Basgall_Programador.pdf?alt=media&token=4e931bee-ba52-472e-a342-1ec13060491d",
+    linkedin: "https://www.linkedin.com/in/juanangelbasgall/",
+    github: "https://github.com/d3vh4n5"
+}
