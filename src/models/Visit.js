bea3a834d4e029117a4fe10b4bfb0629cbe225ae
Juan <juanangelbasgall@hotmail.com>
2024-02-02 14:34:39 -0300

    Visitas reparadas y restringidas

--- src/models/Visit.js
+++ src/models/Visit.js
@@ -1,28 +1,10 @@
 import { db } from '../firebase/InitialConfig'
 import { collection, addDoc } from 'firebase/firestore';
 
-const ipInfoApiKey = import.meta.env.PUBLIC_IPINFO_API_KEY
-
-async function getLocationInfo(ip) {
-    const apiUrl = `https://ipinfo.io/${ip}?token=${ipInfoApiKey}`;
-
-    try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error('Error al obtener la información de ubicación:', error);
-    }
-}
-
-const createVisit = async ( locationInfo = null )=>{
-    // const { city, region, country } = locationInfo
+const createVisit = async ()=>{
     const visit = {
         origin: "Portfolio - Astro",
         date: new Date(),
-        // city: city,
-        // region: region,
-        // country: country
     }
     try{   
         const docRef = await addDoc(collection(db, 'visits'), visit)
@@ -38,7 +20,6 @@ export const visitHandler = ()=>{
         .then(data => {
             if (data.ip !== import.meta.env.PUBLIC_AVOID || window.location.hostname == 'localhost'){
                 try{
-                    // const locationInfo = getLocationInfo(data.ip)
                     createVisit()
                 } catch (e) {
                     console.warn('Visit not counted')
